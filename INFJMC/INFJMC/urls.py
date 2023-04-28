cf7223d5afa515e39f6cef04b631842b18ca84e8
AlumnosInf <AlumnosInf@soportetiu.local>
2023-04-28 12:01:05 -0400

    clase4

--- INFJMC/INFJMC/urls.py
+++ INFJMC/INFJMC/urls.py
@@ -16,7 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from core import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.home, name="home" ),
+    path('docentes/', views.docentes, name="docentes"),
+    path('carreras/', views.carreras, name="carreras")
 ]
--- INFJMC/core/views.py
+++ INFJMC/core/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+def home(request):
+    return render(request,'core/home.html')
+                #HttpResponse(
+                        #"<h1>hola mundo, este es el home </h1>"+
+                        #"<h4><a href=http://127.0.0.1:8000/carreras>ver carreras </a></h4>"+
+                        #"<h4><a href=http://127.0.0.1:8000/docentes>ver docentes </a></h4>")
+
+def docentes(request):
+    return HttpResponse("<h1>Docentes</h1>"+
+                        "<h4><a href=http://127.0.0.1:8000>volver a home </a></h4>"+
+                        "<h4><a href=http://127.0.0.1:8000/carreras>ver carreras </a></h4>"
+                        )
+
+def carreras(request):
+    return HttpResponse("<h1>Carreras</h1>"+
+                        "<h4><a href=http://127.0.0.1:8000>volver a home </a></h4>"+
+                        "<h4><a href=http://127.0.0.1:8000/docentes>ver docentes </a></h4>"
+                        )
