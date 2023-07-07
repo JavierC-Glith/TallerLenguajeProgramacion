57a4a97f29f231b1ab439cf76d8f1cdbbeb6a270
JavierCueto <javier.cuetof@sansano.usm.cl>
2023-07-07 05:06:50 -0400

    Certamen3

--- SVM/rest/views.py
+++ SVM/rest/views.py
@@ -2,7 +2,10 @@ from django.shortcuts import render
 from rest_framework import viewsets
 from .serializers import HabitacionSerializer
 from core.models import Habitacion
+from django.http import JsonResponse
 
 class HabitacionViewSet(viewsets.ModelViewSet):
-    queryset = Habitacion.objects.all()
+    queryset = Habitacion.objects.filter(ocupada=False)
     serializer_class= HabitacionSerializer
+
+
